6c9a6562ea0fce4f3855f73d98a238e585507488
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-01-27 20:06:20 +0300

    create two spinners and display the current currency

--- Currency_Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ Currency_Converter/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -12,60 +12,103 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class MainActivity extends AppCompatActivity{ //implements AdapterView.OnItemSelectedListener{
 
 
-        int  userInput;
-        Button btn;
+    TextView displayResults2;
+    TextView displayResults;
+    int  userInput;
+    Button btn;
         int counter = 0;
+        String currencies[] = {"ETB", "USD", "GBP", "EUR"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner fromSpin = findViewById(R.id.from);
+
+
+
+        displayResults = findViewById(R.id.displayResult);
+        displayResults2 = findViewById(R.id.displayResult2);
+
         Spinner toSpin = findViewById(R.id.toS);
+//        toSpin.setOnItemSelectedListener(this);
+        //creating array adapter from the currencies string.
+        ArrayAdapter arAd = new ArrayAdapter(this, android.R.layout.simple_spinner_item,currencies);
+        arAd.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        toSpin.setAdapter(arAd);
 
-        ArrayAdapter<CharSequence> toCharsAdapter = ArrayAdapter.createFromResource(this,R.array.fromC, android.R.layout.simple_spinner_item);
-        toCharsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        toSpin.setAdapter(toCharsAdapter);
-        toSpin.setOnItemSelectedListener(this);
+        toSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    displayResults2.setText((CharSequence) parent.getItemAtPosition(position));
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+//        Spinner toSpin = findViewById(R.id.toS);
+//        ArrayAdapter<CharSequence> toCharsAdapter = ArrayAdapter.createFromResource(this,R.array.fromC, android.R.layout.simple_spinner_item);
+//        toCharsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        toSpin.setAdapter(toCharsAdapter);
+//        toSpin.setOnItemSelectedListener(this);
+
+        Spinner fromSpin = findViewById(R.id.from);
+        //creating the array adapters from resources
         ArrayAdapter<CharSequence> fromCharsAdapter = ArrayAdapter.createFromResource(this,R.array.fromC, android.R.layout.simple_spinner_item);
+
         fromCharsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         fromSpin.setAdapter(fromCharsAdapter);
-        fromSpin.setOnItemSelectedListener(this);
+//        fromSpin.setOnItemSelectedListener(this);
+        fromSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                displayResults.setText((CharSequence) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
-    public void conv(View v){
-        View butonVw = findViewById(R.id.button);
-        Button button = (Button) butonVw;
-        button.setText("Hi");
 
 
-        View butonVw2 = findViewById(R.id.button2);
-        Button buton2 = (Button) butonVw2;
 
-        if(counter%2 == 0){
-            buton2.setText("Even");
-            Toast.makeText(this,"Clicked",Toast.LENGTH_LONG).show();
-        }
-        else{
-            buton2.setText("Odd");
-
-        }
-
-        counter++;
-        /**
-        v.setEnabled(false);
-        Button button = (Button) v;
-        button.setText("HI");
-        **/
-
-    }
+//    public void conv(View v){
+//        View butonVw = findViewById(R.id.button);
+//        Button button = (Button) butonVw;
+//        button.setText("Hi");
+//
+//
+//        View butonVw2 = findViewById(R.id.button2);
+//        Button buton2 = (Button) butonVw2;
+//
+//        if(counter%2 == 0){
+//            buton2.setText("Even");
+//            Toast.makeText(this,"Clicked",Toast.LENGTH_LONG).show();
+//        }
+//        else{
+//            buton2.setText("Odd");
+//
+//        }
+//
+//        counter++;
+//        /**
+//        v.setEnabled(false);
+//        Button button = (Button) v;
+//        button.setText("HI");
+//        **/
+//
+//    }
     public void calculate(View v){
         EditText input = findViewById(R.id.take_input);
-        TextView displayResults = findViewById(R.id.displayResult);
         int getNumber = Integer.parseInt(input.getText().toString()); //taking the input from the number and storing it.
 
         String numberDisplay = String.valueOf(getNumber); //converting the number into string to display it.
@@ -77,15 +120,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+//    @Override
+//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//        String text = parent.getItemAtPosition(position).toString();
+//        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+////        Toast.makeText(getApplicationContext(),currencies[position],Toast.LENGTH_LONG).show();
 //
+//
+////
+//    }
+//
+////    public void onItemSelected(AdapterView<?> arg0,View arg1,int position,long id)
+////    {
+////        // make toastof name of course
+////        // which is selected in spinner
+////        Toast.makeText(getApplicationContext(),currencies[position],Toast.LENGTH_LONG).show();
+////    }
+//
+//    @Override
+//    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-}
